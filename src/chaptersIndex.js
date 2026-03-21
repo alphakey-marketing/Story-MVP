f265f40a6d45f68502e26ee730e6e6475da4d0af
alphakey-marketing <curryfishcake@gmail.com>
2026-03-21 16:08:15 +0800

    Create src/chaptersIndex.js file with chapter imports

---
+++ src/chaptersIndex.js
@@ -0,0 +1,13 @@
+import { chapter as chapter1 } from './chapters/chapter1.js';
+import { chapter as chapter2 } from './chapters/chapter2.js';
+import { chapter as chapter3 } from './chapters/chapter3.js';
+import { chapter as chapter4 } from './chapters/chapter4.js';
+import { chapter as chapter5 } from './chapters/chapter5.js';
+import { chapter as chapter6 } from './chapters/chapter6.js';
+import { chapter as chapter7 } from './chapters/chapter7.js';
+import { chapter as chapter8 } from './chapters/chapter8.js';
+
+export const chapters = [
+  chapter1, chapter2, chapter3, chapter4,
+  chapter5, chapter6, chapter7, chapter8
+];
